1cd6c51a43982633aff45c09e49901752522852d
liuzhuang7 <liuzhuang7@tal.com>
2023-09-01 15:12:41 +0800

    set 7000 chinese

--- chars.py
+++ chars.py
@@ -1,4 +1,11 @@
 import string
+import csv
+
+# usualUniCode_3500 = \
+#     [ord(i) for i in string.printable[:-1]] + \
+#     [i for i in range(0x4e00, 0x9fff+1, 1)]
+
 usualUniCode_3500 = \
     [ord(i) for i in string.printable[:-1]] + \
-    [i for i in range(0x4e00, 0x9fff+1, 1)]
+    [ord(i[1]) for i in csv.reader(open('7000通用汉字.csv'))]
+
