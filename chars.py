ff70f9e7d85ae01ff3b6ac6b060cfe353418d72a
liuzhuang7 <liuzhuang7@tal.com>
2023-09-04 09:48:04 +0800

    add 3500 chinese chars

--- chars.py
+++ chars.py
@@ -7,5 +7,7 @@ import csv
 
 usualUniCode_3500 = \
     [ord(i) for i in string.printable[:-1]] + \
+    [ord(i) for i in csv.reader(open('3500通用汉字.csv'))] + \
     [ord(i[1]) for i in csv.reader(open('7000通用汉字.csv'))]
 
+usualUniCode_3500 = list(set(usualUniCode_3500))
